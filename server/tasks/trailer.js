1dadace7b4b816359926f7110c871c426b2126cc
markwang2764 <276410287@qq.com>
2018-02-22 22:52:59 +0800

    feat:新增预告片详情页

--- server/tasks/trailer.js
+++ server/tasks/trailer.js
@@ -1,18 +1,26 @@
-const child = cp.fork(script,[])
-let invoked = false
+const cp = require('child_process')
+const { resolve } = require('path')
 
-child.on('error', err => {
-  if(invoked) return
-  invoked = true
-  console.log(err)
-})
-child.on('exit',code => {
-  if(invoked) return
-  invoked = false
-  let err = code === 0 ? null : new Error('exit code' + code)
-  console.log(err)
-})
+;(async () => {
+  const script = resolve(__dirname,'../crawler/video')
+  console.log(script)
+  const child = cp.fork(script,[])
+  let invoked = false
 
-child.on("message", data => {
-  console.log(data)
-})
+  child.on('error', err => {
+    if(invoked) return
+    invoked = true
+    console.log(err)
+  })
+  child.on('exit',code => {
+    if(invoked) return
+    invoked = false
+    let err = code === 0 ? null : new Error('exit code' + code)
+    console.log(err)
+  })
+  
+  child.on("message", data => {
+    console.log(2)
+    console.log(data)
+  })
+})()
