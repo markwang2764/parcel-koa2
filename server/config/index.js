255917791fb1b009005eced0cbfbb036dd1dc01c
markwang2764 <276410287@qq.com>
2018-02-23 10:26:33 +0800

    feat: 增加时间模型代码示例

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "qiniu":{
+    "bucket": "mark-ymd",
+    "video": "http://olvau7qfq.bkt.clouddn.com/",
+    "AK": "Hxy0nzABuypGcgS2IIEz--8U9tn8oP99OjRz9Bbk",
+    "SK": "yHxnp-0hUj_V1NvjOZjq80k7LoB4zDpPdQnXbCHQ"
+  }
+}
--- server/crawler/video.js
+++ server/crawler/video.js
@@ -1,7 +1,7 @@
 const puppeteet = require('puppeteer')
 const base = `https://movie.douban.com/subject/`
 const doubanId = '25875034'
-const videoBase = `https://movie.douban.com/trailer/227429/#content`
+const videoBase = `https://movie.douban.com/trailer/227429`
 const sleep = time => new Promise(resolve => {
   setTimeout(resolve,time)
 })
@@ -17,11 +17,13 @@ const sleep = time => new Promise(resolve => {
     wartUntil: 'networkidle2'
   })
   await sleep(1000)
-  await page.waitForSelector('.more')
  
   const result = await page.evaluate(() => {
     var $ = window.$
+
     var it = $('.related-pic-video')
+    console.log(1)
+    console.log(it)
     if(it && it.length>0){
       var link = it.attr('href')
       var cover = it.find('img').attr('src')
@@ -31,25 +33,35 @@ const sleep = time => new Promise(resolve => {
     }
     return {}
   })
+  console.log(result)
   let video
   if (result.link) {
+    console.log(result.link)
     await page.goto(result.link,{
       waitUntil: 'networkidle2'
     })
+    console.log(1)
+    await sleep(2000)
+    console.log(2)
     video = await page.evaluate(() => {
+      console.log(window)
       var $ = window.$
+      console.log(window.$)
       var it = $('source')
+      console.log(it)
       if (it && it.length > 0) {
         return it.attr('src')
       }
+      return ''
     })
-    return ''
   }
   const data = {
     video,
     doubanId,
     cover: result.cover
   }
+  console.log(video);
+  console.log(data)
   browser.close()
   process.send(data)
   process.exit(0)
